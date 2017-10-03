dc8e6b6f4fae82cda5ea19af93349b48cda28084
FaizeChan <obey.faize@gmail.com>
2017-10-03 11:42:13 -0400

    initial commit

--- Week2_day2Class/Program.cs
+++ Week2_day2Class/Program.cs
@@ -11,19 +11,21 @@ namespace Week2_day2Class
         static void Main(string[] args)
         {
 
-            string[] firstNames = { "Faizon", "Posiden", "Zues", "Amaterasu", "Micheal", "Lucifer" };
-            Console.WriteLine(Array.IndexOf(firstNames, "Faizon"));
+            //string[] firstNames = { "Faizon", "Posiden", "Zues", "Amaterasu", "Micheal", "Lucifer" };
+            //Console.WriteLine(Array.IndexOf(firstNames, "Faizon"));
 
-            int[] luckyNumber = { 1, 5, 7, 22, 44, 25 };
-            Console.WriteLine(Array.LastIndexOf(luckyNumber, 7));
+            //int[] luckyNumber = { 1, 5, 7, 22, 44, 25 };
+            //Console.WriteLine(Array.LastIndexOf(luckyNumber, 7));
 
-            char[] alphebetCharacters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-            Console.WriteLine(alphebetCharacters[12]);
+            //char[] alphebetCharacters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            //Console.WriteLine(alphebetCharacters[12]);
 
-            string[] studentNames = { "JD", "Raz", "David", "Dan", "Monica", "Faizon", "Brian" };
-            Array.Sort(studentNames);
-            Console.WriteLine(studentNames[0]);
-            Console.WriteLine(studentNames[studentNames.Length -1]);
+            //string[] studentNames = { "JD", "Raz", "David", "Dan", "Monica", "Faizon", "Brian" };
+            //Array.Sort(studentNames);
+            //Console.WriteLine(studentNames[0]);
+            //Console.WriteLine(studentNames[studentNames.Length -1]);
+
+
 
 
 
